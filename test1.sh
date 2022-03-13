7a031c610902353202b63fffda59ceb31c7dbc04
Ivan Usalko <ivict@rambler.ru>
2022-03-13 22:23:26 +0300

    # INITIAL
      --  There is the refactoring (rename script).
      --  There is the test for send smtp through tls.

---
+++ test1.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+function read_input {
+    echo $@
+    read INPUT_CONTENT
+    RETURN_VALUE=$INPUT_CONTENT
+}
+
+function read_password {
+    read -sp "$@" INPUT_CONTENT
+    RETURN_VALUE=$INPUT_CONTENT
+}
+
+read_input Имя почтового сервера, при нажатии Enter без ввода имени будет подствалено значение gmail.com:
+SERVER_NAME=$RETURN_VALUE
+read_input Имя пользователя почтового сервера ваш email:
+USER_NAME=$RETURN_VALUE
+read_input Введите пароль:
+SMTP_PASSWORD=$RETURN_VALUE
+
+echo $SERVER_NAME
+echo $USER_NAME
+echo $SMTP_PASSWORD
+
+#PORTS
+# Port 25, 587, 465, or 2525)
+
+subject="Subject of my email"
+txtmessage="This is the message I want to send"
+username='mail@exemple.com'
+password='************'
+From="mail01@exemple.com"
+rcpt='testemail5739230@yopmail.com'
+from_name='N BOINA'
+rcpt_name='Nicolas Hulot'
+
+{
+sleep 0.3;
+echo "EHLO $(echo $username | cut -d "@" -f 2 )"
+sleep 0.3;
+
+# Comment this line if you use smtp without authentication
+echo "AUTH PLAIN $(echo -ne "\0$username\0$password" | base64)"
+sleep 0.3;
+
+## uncomment if using smtp without encryption with telnet
+#echo "AUTH LOGIN"
+#sleep 0.3;
+#echo $(echo -ne $username | base64) && echo $username64 
+#sleep 0.3;
+#echo $(echo -ne "\0$username\0$password" | base64)
+
+sleep 0.3;
+echo "MAIL FROM:<$From>"
+
+sleep 0.3;
+echo "rcpt to:<$rcpt>"
+
+sleep 0.3;
+echo "DATA"
+
+sleep 0.3
+echo "Subject: $subject"
+
+sleep 0.3
+echo "MIME-Version: 1.0"
+
+sleep 0.3
+echo "From: $from_name <$From>"
+
+sleep 0.3
+echo "Content-Type: text/html; charset=”UTF-8″"
+
+sleep 0.3
+echo "To: $rcpt_name <$rcpt>"
+
+sleep 0.3
+echo "Content-Transfer-Encoding: quoted-printable"
+
+sleep 0.3
+echo $txtmessage
+
+sleep 0.3;
+echo "."
+
+sleep 0.3;
+
+## uncomment one of the following line depending on the encryption mode you want to use.
+
+#} | telnet smtp-relay.gmail.com 25
+#} | openssl s_client -starttls smtp -crlf -connect smtp-relay.gmail.com:587
+} | openssl s_client -crlf -connect smtp-relay.gmail.com:465
